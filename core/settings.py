3fb5539af4b28344ec41ae44e2a2a7147e83ffb5
Jonas Maia <jonasgmaia2@gmail.com>
2026-05-11 15:42:46 -0300

    correction for render 3

--- core/settings.py
+++ core/settings.py
@@ -171,4 +171,4 @@ DEBUG = os.environ.get("DEBUG", "0") == "1"
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1 localhost").split(" ")
+# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1 localhost").split(" ")
